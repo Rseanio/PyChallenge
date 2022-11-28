0c5cbc550bef5806c0cc496c5b7273d135c11bc9
Rseanio <mrrealizze@gmail.com>
2022-11-28 10:52:52 +0000

    Completed challenges

--- Challenge1.py
+++ Challenge1.py
@@ -8,5 +8,5 @@ for x in string:
         newstring = newstring + " "
     else:
         newstring = newstring + (dict[x])
-print(newstring)
+#print(newstring)
 #i hope you didnt translate it by hand. thats what computers are for. doing it in by hand is inefficient and that's why this text is so long. using string.maketrans() is recommended. now apply on the url.
